46f051d113fccdef4ead4248cf1c1b8dbeb6ad67
Pavel Korolev <dev@borodust.org>
2018-04-29 21:07:55 +0300

    Add better startup screen

--- src/game/start.lisp
+++ src/game/start.lisp
@@ -13,4 +13,8 @@
 
 (defmethod render ((this start-state))
   (with-slots () this
-    (gamekit:draw-text "Press ENTER to begin" (gamekit:vec2 10 10))))
+    (gamekit:with-pushed-canvas ()
+      (gamekit:scale-canvas 2 2)
+      (gamekit:draw-text "BALL-Z" (gamekit:vec2 160 200))
+      (gamekit:draw-text "Second Dimension" (gamekit:vec2 100 160)))
+    (gamekit:draw-text "Press ENTER to begin" (gamekit:vec2 290 150))))
